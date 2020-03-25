5b0ab32900e994e6d584cfb9ec250890f4c38aac
Igor Marcelo Bezerra <igor.marcelo.bezerra@hotmail.com>
2020-03-25 18:39:48 -0300

    Updates

---
+++ precache-manifest.cd28baacde44e0fbd2327fe03578ea61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddec1458196a2a0e84525996124aed31",
+    "url": "/ReactApp_Redux/index.html"
+  },
+  {
+    "revision": "ff991b2cbb833923de28",
+    "url": "/ReactApp_Redux/static/js/2.e5649836.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactApp_Redux/static/js/2.e5649836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b23a8b723cbfe85f682",
+    "url": "/ReactApp_Redux/static/js/main.3d9b6353.chunk.js"
+  },
+  {
+    "revision": "c6d08b00ce52c3fc2dac",
+    "url": "/ReactApp_Redux/static/js/runtime-main.e21ccceb.js"
+  },
+  {
+    "revision": "ce2e42e9ce9af0c34204c06237380edf",
+    "url": "/ReactApp_Redux/static/media/Logo.ce2e42e9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.183a27ed13ab542e3188a326e823cae0.js"
+  "/ReactApp_Redux/precache-manifest.cd28baacde44e0fbd2327fe03578ea61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactApp_Redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
