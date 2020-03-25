ff7d2e49cbf59c7d57a23918fcd9e9ab24e2d4cc
Igor Marcelo Bezerra <igor.marcelo.bezerra@hotmail.com>
2020-03-25 18:34:17 -0300

    Updates

---
+++ precache-manifest.183a27ed13ab542e3188a326e823cae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82a72e76636536eb651709b7b99aa4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22568ad00c98cee22711",
+    "url": "/static/js/2.c4b98221.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4b98221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b23a8b723cbfe85f682",
+    "url": "/static/js/main.3d9b6353.chunk.js"
+  },
+  {
+    "revision": "24d0525bd9f86b951d2c",
+    "url": "/static/js/runtime-main.7d25204c.js"
+  },
+  {
+    "revision": "ce2e42e9ce9af0c34204c06237380edf",
+    "url": "/static/media/Logo.ce2e42e9.png"
+  }
+]);
